cd8498778a8441adecc34644e88f4f0cf2d3bdd4
Marina-Filatova <78900635+Marina-Filatova@users.noreply.github.com>
2021-04-04 21:45:52 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -33,33 +33,40 @@ return count;
 }
 
 int countPairs3(int *arr, int len, int value) {
-int count = 0, left = 0, right = len;
-while (true)
-{
-if ((arr[right] > value) || (arr[left] + arr[right] > value)) {
-right--;
-}
-else {
-break;
-}
-}
-  
-len = right;
-for (int i = 0; i < len; i++) {
-left = i + 1, right = len - 1;
-int f = 0;
-while (left < right) {
-int middle = (left + right) / 2;
-if (arr[middle] < (value - arr[i]))
+int left = 0, right = len, middle;
+while (left < right - 1) {
+middle = (left + right) / 2;
+if (arr[middle] <= value)
 left = middle + 1;
 else
 right = middle;
 }
-while (arr[left] == (value - arr[i])) {
-f++;
-left++;
+len = right;
+  
+int count = 0;
+for (int i = 0; i < len - 1;i++) {
+int left = i, right = len;
+while (left < right - 1) {
+int middle = (left + right) / 2;
+if (arr[i] + arr[middle] == value) {
+count++;
+int j = middle + 1;
+while (arr[i] + arr[j] == value && j < right) {
+count++;
+j++;
 }
-count += f;
+j = middle - 1;
+while (arr[i] + arr[j] == value && j > left) {
+count++;
+j--;
+}
+break;
+}
+if (arr[i] + arr[middle] > value)
+right = middle;
+else
+left = middle;
+}
 }
 return count;
 }
